555d87657fd5e025107f40c03b671c7a5f0e5f3e
samstronghammer <oliphantsb@gmail.com>
2023-12-06 08:43:34 -0500

    day 06

--- lib/util/file_util.ex
+++ lib/util/file_util.ex
@@ -1,4 +1,14 @@
 defmodule FileUtil do
+
+  @spec extract_numbers_from_line(String.t()) :: Enumerable.t(integer)
+  defp extract_numbers_from_line(line) do
+    Regex.scan(~r/\d+/, line, capture: :first) |> Enum.map(&List.first/1) |> Enum.map(&String.to_integer/1)
+  end
+  
+  @spec extract_numbers(Enumerable.t(String.t())) :: Enumerable.t(Enumerable.t(integer))
+  def extract_numbers(lines) do
+    lines |> Enum.map(&(extract_numbers_from_line(&1)))
+  end
   
   def parse_file() do
     
