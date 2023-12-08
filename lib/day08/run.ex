e9a24f52c55bcb5bc63f181c3014c5ddffc40452
samstronghammer <oliphantsb@gmail.com>
2023-12-08 09:12:18 -0500

    Day 08

---
+++ lib/day08/run.ex
@@ -0,0 +1,35 @@
+
+defmodule AdventOfCode.Day08 do
+  use AdventOfCode.FileUtils
+  alias AdventOfCode.FileUtils
+
+  def ends_with?(s, letter) do
+    String.last(s) === letter
+  end
+
+  def cycle_length(start_node, pattern, node_map) do
+    pattern |> Stream.cycle() |> Enum.reduce_while({start_node, 0}, fn lr, {node, count} -> 
+      if ends_with?(node, "Z") do
+        {:halt, count}
+      else
+        {:cont, {node_map[node][lr], count + 1}}
+      end
+    end)
+  end
+
+  def run() do
+    lines = puzzle_lines()
+    pattern = List.first(lines) |> String.to_charlist
+    nodes = Enum.slice(lines, 1..-1//1) |> Enum.map(&FileUtils.extract_regex_from_line(&1, ~r/[A-Z]{3}/))
+    node_map = Map.new(nodes, fn node_list -> 
+      {List.first(node_list), %{?L => Enum.at(node_list, 1), ?R => Enum.at(node_list, 2)}}
+    end)
+    IO.inspect cycle_length("AAA", pattern, node_map)
+    IO.inspect nodes |> Enum.map(&List.first/1) 
+                     |> Enum.filter(&ends_with?(&1, "A")) 
+                     |> Enum.map(&cycle_length(&1, pattern, node_map))
+                     |> Enum.reduce(1, &Math.lcm/2)
+  end
+end
+
+
--- lib/util/file_util.ex
+++ lib/util/file_util.ex
@@ -16,9 +16,14 @@ defmodule AdventOfCode.FileUtils do
 
   @spec extract_numbers_from_line(String.t()) :: Enumerable.t(integer)
   def extract_numbers_from_line(line) do
-    Regex.scan(~r/\d+/, line, capture: :first) |> Enum.map(&List.first/1) |> Enum.map(&String.to_integer/1)
+    extract_regex_from_line(line, ~r/\d+/) |> Enum.map(&String.to_integer/1)
   end
-  
+
+  @spec extract_regex_from_line(String.t(), Regex.t()) :: Enumerable.t(String.t())
+  def extract_regex_from_line(s, r) do
+    Regex.scan(r, s, capture: :first) |> Enum.map(&List.first/1)
+  end
+
   @spec extract_numbers(Enumerable.t(String.t())) :: Enumerable.t(Enumerable.t(integer))
   def extract_numbers(lines) do
     lines |> Enum.map(&(extract_numbers_from_line(&1)))
